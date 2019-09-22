504922903cc70e197238f372eae256e022bf311c
kensuke goto <utogoken2@yahoo.co.jp>
2019-09-22 20:48:06 +0900

    [追加] POSTでjsonを返却

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
 const PORT = 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cookieParser());
+const SESSION_SECRET = "qwerty";
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "ssiidd"
+}));
+
 
 app.use(bodyParser.urlencoded({extended:true})) // フォームからのPOSTを扱いやすい形式に
 app.use(bodyParser.json()) // JSONがPOSTsれた場合に扱いやすい形式に
+
 
 app.use("/",require("./router.js"))
 app.listen(PORT,()=>{
